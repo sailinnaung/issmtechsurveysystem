51dd7ce18eef8b2bab2da410b21d358f27401f00
viv.shankar@gmail.com <viv.shankar@gmail.com@e08fa17c-ae71-7c13-d8f2-b99878eddebe>
2011-01-15 05:45:53 +0000

    Created the dummy beans for respondent and researcher. Removed SurveyFacadeBean.

---
+++ trunk/Code/SurveyEJB/src/java/survey/bpo/ResearcherSurveyFacadeBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package survey.bpo;
+
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+import survey.dto.*;
+
+/**
+ *
+ * @author vivek
+ */
+@Stateless
+public class ResearcherSurveyFacadeBean implements ResearcherSurveyFacadeRemote {
+
+    public QuestionDTO createQuestion(int surveyID, int surveyPageID, QuestionDTO question) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public SurveyDTO createSurvey(String username, SurveyDTO survey) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public SurveyPageDTO createSurveyPage(int surveyID, SurveyPageDTO surveyPage) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean deleteQuestion(int surveyID, int surveyPageID, int questionID) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean deleteSurvey(String username, int surveyID) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public boolean deleteSurveyPage(int surveyID, int surveyPageID) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ArrayList<SurveyDTO> findSurveys(String username, SurveySearchCriteriaDTO criteria) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public ArrayList<SurveyDTO> findSurveysByState(String username, ActivityTypes state) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public QuestionDTO updateQuestion(int surveyID, int surveyPageID, QuestionDTO question) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public SurveyDTO updateSurvey(String username, SurveyDTO survey) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public SurveyPageDTO updateSurveyPage(int surveyID, SurveyPageDTO surveyPage) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
