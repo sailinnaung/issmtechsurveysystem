43d9c81e0f39ae518d552075e75745192f582681
viv.shankar@gmail.com <viv.shankar@gmail.com@e08fa17c-ae71-7c13-d8f2-b99878eddebe>
2011-01-15 02:35:11 +0000

    Updates to the existing DTOs

--- Code/SurveyEJB/src/java/survey/dto/SurveyPageDTO.java
+++ Code/SurveyEJB/src/java/survey/dto/SurveyPageDTO.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  */
 public class SurveyPageDTO implements Serializable {
 
+    private int surveyPageID;
     private int pageNo;
     private String title;
     private String description;
