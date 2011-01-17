808944df8074c5181d4237fa43b8f88a47864ec1
slmmaung <slmmaung@e08fa17c-ae71-7c13-d8f2-b99878eddebe>
2011-01-17 16:39:28 +0000

    UI

--- Code/SurveyWeb/src/java/survey/delegates/ResearcherSurveyDelegate.java
+++ Code/SurveyWeb/src/java/survey/delegates/ResearcherSurveyDelegate.java
@@ -7,12 +7,15 @@ package survey.delegates;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import survey.bpo.ResearcherSurveyFacadeRemote;
-import survey.dto.ActivityTypes;
+import survey.dto.OptionDTO;
 import survey.dto.QuestionDTO;
 import survey.dto.SurveyDTO;
 import survey.dto.SurveyPageDTO;
+import survey.dto.SurveySearchCriteriaDTO;
 import survey.exception.DAOException;
 import survey.exception.InvalidFieldException;
 import survey.exception.OperationFailedException;
@@ -44,11 +47,35 @@ public class ResearcherSurveyDelegate {
         return rshrFacade.createSurvey(ownerName, survey);
     }
     
+    public SurveyDTO updateSurvey(String ownerName,SurveyDTO survey) throws UserNotFoundException, 
+            UserNotAllowedException, RecordNotFoundException, InvalidFieldException
+    {
+        return rshrFacade.updateSurvey(ownerName, survey);
+    }
+    
+    public boolean deleteSurvey(String ownerName,int surveyID) throws UserNotFoundException, 
+            UserNotAllowedException, RecordNotFoundException
+    {
+        return rshrFacade.deleteSurvey(ownerName, surveyID);
+    }
+    
     public SurveyPageDTO createSurveyPage(String ownerName,int surveyID, SurveyPageDTO surveyPage) 
             throws UserNotFoundException, OperationFailedException, RecordNotFoundException, 
             InvalidFieldException, UserNotAllowedException
     {
         return rshrFacade.createSurveyPage(ownerName, surveyID, surveyPage);
+    }
+    
+    public SurveyPageDTO updateSurveyPage(String ownerName, int surveyID,SurveyPageDTO surveyPage) throws UserNotFoundException, 
+            RecordNotFoundException, InvalidFieldException, UserNotAllowedException
+    {
+        return rshrFacade.updateSurveyPage(ownerName, surveyID, surveyPage);
+    }
+    
+    public boolean deleteSurveyPage(String ownerName, int surveyID, int surveyPageID) throws UserNotFoundException, 
+            RecordNotFoundException, UserNotAllowedException
+    {
+        return rshrFacade.deleteSurveyPage(ownerName, surveyID, surveyPageID);
     }
     
     public QuestionDTO createQuestion(String ownerName,int surveyID, int surveyPageID, QuestionDTO questDto) 
@@ -57,6 +84,51 @@ public class ResearcherSurveyDelegate {
     {
         return rshrFacade.createQuestion(ownerName, surveyID, surveyPageID, questDto);
     }
+    
+    public QuestionDTO updateQuestin(String ownerName, int surveyID, int surveyPageID, QuestionDTO question) throws UserNotFoundException, 
+            RecordNotFoundException, InvalidFieldException, UserNotAllowedException
+    {
+        return rshrFacade.updateQuestion(ownerName, surveyID, surveyPageID, question);
+    }
+    
+    public boolean deleteQuestion(String ownerName,int surveyID, int surveyPageID,int questionID) throws UserNotFoundException, 
+            UserNotFoundException, UserNotAllowedException, RecordNotFoundException
+    {
+        return rshrFacade.deleteQuestion(ownerName, surveyID, surveyPageID, questionID);
+    }
+    
+    public OptionDTO createQuestionOption(String ownerName,int questionID, OptionDTO option) throws UserNotFoundException, 
+            OperationFailedException, RecordNotFoundException, InvalidFieldException, UserNotAllowedException
+    {
+        return rshrFacade.createQuestionOption(ownerName, questionID, option);
+    }
+    
+    public OptionDTO updateQuestionOption(String ownerName, int questionID, OptionDTO option) throws UserNotFoundException,
+            RecordNotFoundException, InvalidFieldException
+    {
+        return rshrFacade.updateQuestionOption(ownerName, questionID, option);
+    }
+    
+    public boolean deleteQuestionOption(String ownerName,int questionID, OptionDTO option) throws UserNotFoundException,
+            RecordNotFoundException
+    {
+        return rshrFacade.deleteQuestionOption(ownerName, questionID, option);
+    }
+    
+    public SurveyDTO getSurvey(String ownerName, String surveyCode) throws UserNotFoundException
+    {
+        return rshrFacade.getSurvey(ownerName, surveyCode);
+    }
+    
+    public SurveyDTO getSurvey(String ownerName,int surveyID) throws UserNotFoundException
+    {
+        return rshrFacade.getSurvey(ownerName, surveyID);
+    }
+    
+    public ArrayList<SurveyDTO> findSurveys(String ownerName,SurveySearchCriteriaDTO criteria)
+    {
+        return rshrFacade.findSurveys(ownerName, criteria);
+    }
 
     public ArrayList<SurveyDTO> findSurveysByState(String username, int state)
             throws DAOException, RemoteException
@@ -64,10 +136,11 @@ public class ResearcherSurveyDelegate {
         return rshrFacade.findSurveysByState(username, state);
     }
 
-    public ArrayList<SurveyDTO> findRecentSurveysByState(String username,int state,int count)
+    public List<SurveyDTO> findRecentSurveysByState(String username,int state,int count)
         throws DAOException, RemoteException
     {
         System.out.println("user name in findRecentSurveyByState is "+username+" and state is "+state);
+        
         ArrayList<SurveyDTO> allListByState = rshrFacade.findSurveysByState(username, state);
         ArrayList<SurveyDTO> recentListByState = new ArrayList<SurveyDTO>();
         System.out.println("inside findRecentSurveyByState after retrieving from remote bean.");
--- Code/SurveyWeb/web/Questionnaire_new.jsp
+++ Code/SurveyWeb/web/Questionnaire_new.jsp
@@ -19,34 +19,61 @@
     </head>
     <body>
         <fmt:setBundle basename="SurveyResource"/>
-        <s:form namespace="/questionnaire" id="questionnaire_new_form" action="submitQuestionniare_survey" method="post">	
+        <s:form namespace="/questionnaire" id="questionnaire_new_form" action="createQuestionniare_survey" method="post">	
 	<div id="questionnaire_new_details" align="center">
-		<table cellpadding="0" cellspacing="0" width="100%" border="0">
-		  <tr>
-			<th scope="row" class="quest_label_new" width="30%">Survey ID :</th>
-                        <td><s:textfield name="currentSurvey.surveyID" size="25%"/></td>
-		  </tr>
-		  <tr>
-			<th scope="row" class="quest_label_new" width="30%">Survey Title :</th>
-			<td><s:textfield name="currentSurvey.title" size="50%"/></td>
-		  </tr>
-		  <tr>
-			<th scope="row" class="quest_label_new" width="30%">Description :</th>
-                        <td><s:textarea name="currentSurvey.description" rows="5"/></td>
-		  </tr>
-		  <tr>			
-                        <td><sx:datetimepicker name="currentSurvey.startDate" cssClass="quest_label_new" label="Publish Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
-		  </tr>
-		  <tr>			
-			<td><sx:datetimepicker name="currentSurvey.startDate" cssClass="quest_label_new" label="Expire Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
-		  </tr>
-	</table>
+               <s:if test="currentSurvey.code==null">
+                <table cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tr style="width:100%">
+                            <th scope="row" class="quest_label_new" width="30%">Survey ID :</th>
+                            <td>                            
+                                <s:textfield name="currentSurvey.code" size="25%" cssClass="mandatory_textBox"/>                        
+                            </td>
+                      </tr>
+                      <tr>
+                            <th scope="row" class="quest_label_new" width="30%">Survey Title :</th>
+                            <td><s:textfield name="currentSurvey.title" size="50%" cssClass="mandatory_textBox"/></td>
+                      </tr>
+                      <tr>
+                            <th scope="row" class="quest_label_new" width="30%">Description :</th>
+                            <td><s:textarea name="currentSurvey.description" rows="5" cols="10"/></td>
+                      </tr>
+                      <tr>			
+                            <td colspan="2"><sx:datetimepicker name="currentSurvey.startDate" cssClass="quest_label_new" label="Publish Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
+                      </tr>
+                      <tr>			
+                            <td colspan="2"><sx:datetimepicker name="currentSurvey.endDate" cssClass="quest_label_new" label="Expire Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
+                      </tr>
+                </table>
+            </s:if>
+            <s:else>
+                <table cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tr style="width:100%">
+                            <th scope="row" class="quest_label_new" width="30%">Survey ID :</th>
+                            <td>                            
+                                <s:textfield name="currentSurvey.code" value="currentSurvey.code" size="25%" cssClass="mandatory_textBox"/>                        
+                            </td>
+                      </tr>
+                      <tr>
+                            <th scope="row" class="quest_label_new" width="30%">Survey Title :</th>
+                            <td><s:textfield name="currentSurvey.title" value="currentSurvey.title" size="50%" cssClass="mandatory_textBox"/></td>
+                      </tr>
+                      <tr>
+                            <th scope="row" class="quest_label_new" width="30%">Description :</th>
+                            <td><s:textarea name="currentSurvey.description" value="currentSurvey.description" rows="5" cols="10"/></td>
+                      </tr>
+                      <tr>			
+                          <td colspan="2"><sx:datetimepicker name="currentSurvey.startDate" value="currentSurvey.startDate" cssClass="quest_label_new" label="Publish Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
+                      </tr>
+                      <tr>			
+                          <td colspan="2"><sx:datetimepicker name="currentSurvey.endDate" value="currentSurvey.endDate" cssClass="quest_label_new" label="Expire Date:(dd-MM-yyyy)" displayFormat="dd-MM-yyyy"/></td>
+                      </tr>
+                </table>
+            </s:else>
 	</div>
 	<div id="questionnaire_new_button" align="center">
 		<table width="100%" border="0" cellpadding="0" cellspacing="0">
 		  <tr>                      
-                      <td  align="center"><s:submit name="submitBtn" value="create" label="Create"/></td>
-                      <td  align="center"><s:submit name="submitBtn" value="submit" label="Submit"/></td>
+                      <td  align="center"><s:submit name="submitBtn" label="Create"/></td>                      
                       <td  align="center"><s:reset label="Reset"/></td>
 		  </tr>                  
 		</table>
